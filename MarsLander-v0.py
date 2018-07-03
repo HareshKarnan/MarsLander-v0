03d5a8b72e934b54c670aaeef0560cecf2117843
haresh.miriyala <haresh.miriyala@gmail.com>
2018-07-03 16:49:41 -0500

    creating env

--- MarsLander-v0.py
+++ MarsLander-v0.py
@@ -4,4 +4,13 @@ from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
 
-class
+class MarsLanderenv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 50
+    }
+
+    def __init__(self):
+        self.gravity = 3.711 #gravity on mars
+        self.mass = 0.1 #mass of each bar in Kg.
+        self.length = 1 #length of each bar in m.
